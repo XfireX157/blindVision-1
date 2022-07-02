dc0f65b636806b8a469c1799682f9a4ffb79bb05
Gustavo <gustavopereirafacal@gmail.com>
2022-07-02 18:11:40 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Page2 } from "./Pages/Page2/index";
 import { Page4 } from "./Pages/Page4/index";
 import { Banner } from "./Pages/Banner/index";
 import { Page3 } from "./Pages/Page3/index";
-import { Footer } from "./Components/footer/index";
+import { Footer } from "./Components/Footer/index";
 export default function App() {
   return (
     <>
